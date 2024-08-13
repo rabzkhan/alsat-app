3a6539bb8dd1b2ee908abdf1f36b2a92c4e6c125
DeveloperOrpon <developerorpon@gmail.com>
2024-08-14 01:08:38 +0600

    Localization l10n

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:get/get.dart';
 import 'app/data/local/my_shared_pref.dart';
 import 'app/routes/app_pages.dart';
 import 'config/theme/app_theme.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 Future<void> main() async {
   // wait for bindings
@@ -20,26 +22,35 @@ Future<void> main() async {
       rebuildFactor: (old, data) => true,
       builder: (context, widget) {
         return GetMaterialApp(
-          title: "ALSAT",
-          useInheritedMediaQuery: true,
-          debugShowCheckedModeBanner: false,
-          builder: (context, widget) {
-            return Theme(
-              data: appTheme(),
-              child: MediaQuery(
-                data: MediaQuery.of(context).copyWith(
-                  textScaler: const TextScaler.linear(1.0),
+            title: "ALSAT",
+            useInheritedMediaQuery: true,
+            debugShowCheckedModeBanner: false,
+            builder: (context, widget) {
+              return Theme(
+                data: appTheme(),
+                child: MediaQuery(
+                  data: MediaQuery.of(context).copyWith(
+                    textScaler: const TextScaler.linear(1.0),
+                  ),
+                  child: widget!,
                 ),
-                child: widget!,
-              ),
-            );
-          },
-          initialRoute: AppPages.INITIAL,
-          getPages: AppPages.routes,
-          theme: appTheme(),
-          // locale: MySharedPref.getCurrentLocal(),
-          // translations: LocalizationService.getInstance(),
-        );
+              );
+            },
+            initialRoute: AppPages.INITIAL,
+            getPages: AppPages.routes,
+            theme: appTheme(),
+            // locale: MySharedPref.getCurrentLocal(),
+            // translations: LocalizationService.getInstance(),
+            locale: Get.deviceLocale,
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: const [
+              Locale('en'), // English
+            ]);
       },
     ),
   );
